63ac60f4dfe51516b82900ec6a23986dd08a5c98
Marco <marco.piraccini@gmail.com>
2021-01-01 11:07:44 +0100

    day 01, recursive solution

--- src/day01.rs
+++ src/day01.rs
@@ -1,27 +1,25 @@
-fn fuel(n: f64) -> f64 {
-    ((n / 3_f64).floor()) - 2_f64 
+use std::cmp;
+
+
+fn fuel(n: i32) -> i32 {
+    (n / 3) - 2 
 }
 
 pub fn part1(input: String) {
-    let masses = input.lines().map(|x| x.parse::<f64>().unwrap());   
-    let sol = masses.fold(0_f64, |acc, x| acc + fuel(x));     
+    let masses = input.lines().map(|x| x.parse::<i32>().unwrap());   
+    let sol = masses.fold(0, |acc, x| acc + fuel(x));     
     println!("Solution part 1: {}", sol)
 }
 
-fn fuel2(n:f64) -> f64 {
-    let mut f = fuel(n);
-    let mut total_fuel = f;
-    while f > 0_f64 {
-        f = fuel(f);
-        if f > 0_f64 { total_fuel += f }
-    }
-    total_fuel
+fn fuel2(n:i32) -> i32 {
+    let f = cmp::max(fuel(n), 0);
+    f + if f > 0 { fuel2(f) } else { 0 }
 }
 
 
 pub fn part2(input: String) {
-    let masses = input.lines().map(|x| x.parse::<f64>().unwrap());   
-    let sol = masses.fold(0_f64, |acc, x| acc + fuel2(x));     
+    let masses = input.lines().map(|x| x.parse::<i32>().unwrap());   
+    let sol = masses.fold(0_i32, |acc, x| acc + fuel2(x));     
     println!("Solution part 2: {}", sol)
 }
 
@@ -32,8 +30,8 @@ mod tests {
 
     #[test]
     fn test_fuel2() {
-        assert_eq!(fuel2(14_f64), 2_f64);
-        assert_eq!(fuel2(1969_f64), 966_f64);
-        assert_eq!(fuel2(100756_f64), 50346_f64);
+        assert_eq!(fuel2(14), 2);
+        assert_eq!(fuel2(1969), 966);
+        assert_eq!(fuel2(100756), 50346);
     }
 }

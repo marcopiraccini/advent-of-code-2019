53c138108ac59918562b279d25d6e91e3208d4ba
marcopiraccini <marco.piraccini@gmail.com>
2021-04-11 15:17:55 +0200

    day 13, part 2

--- src/day13.rs
+++ src/day13.rs
@@ -14,3 +14,34 @@ pub fn part1(input: String) {
 
     println!("Solution: {:?}", nblock);
 }
+
+pub fn part2(mut input: String) {
+    // set 2, play for free!
+    input.replace_range(0..1, "2");
+    let mut program = intcode::get_program(input);
+    let mut paddle_x = 0;
+    let mut score = 0;
+    let mut direction = 0;
+    while !program.done {
+        let x = program.process(direction, false);
+        let y = program.process(direction, false);
+        let tile = program.process(direction, false);
+        if tile == 4 {
+            // ball, decide here the paddle direction
+            if x > paddle_x {
+                direction = 1;
+            } else if x < paddle_x {
+                direction = -1;
+            } else {
+                direction = 0
+            }
+        }
+        if tile == 3 {
+            paddle_x = x;
+        }
+        if x == -1 && y == 0 {
+            score = tile;
+        }
+    }
+    println!("Solution: {:?}", score);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,7 @@ pub fn get_day(day: u32) -> (DayFn, DayFn) {
         10 => (day10::part1, day10::part2),
         11 => (day11::part1, day11::part2),
         12 => (day12::part1, day12::part2),
-        13 => (day13::part1, noop),
+        13 => (day13::part1, day13::part2),
         _ => {
             println!("Unknown day: {}", day);
             (noop, noop)

5af9cfdd29524574c94058cac46c9584ccd2c6ab
marcopiraccini <marco.piraccini@gmail.com>
2021-10-17 09:54:03 +0200

    day 16, part 2

--- src/day16.rs
+++ src/day16.rs
@@ -1,42 +1,32 @@
-use std::collections::VecDeque;
-
-fn fill_pattern(p: &mut Vec<isize>, val: isize, base: usize) -> &mut Vec<isize> {
-    for _ in 0..base {
-        p.push(val);
+// step and pos are from 0
+fn get_pattern_val(step: usize, pos: usize) -> isize {
+    if pos < step {
+        return 0;
     }
-    p
+    let rel_pos = pos - step;
+    let pat_length = 4 * (step + 1);
+    let pat_pos = rel_pos % pat_length;
+    let block = pat_pos / (pat_length / 4);
+    match block {
+        0 => 1,
+        2 => -1,
+        _ => 0,
+    }
 }
 
-fn get_pattern(step: usize, n: usize) -> Vec<isize> {
-    let base = 1 + step; // assume that 0 is the first step
-    let mut ret: VecDeque<isize> = VecDeque::new();
-    let mut pattern: Vec<isize> = vec![];
-    fill_pattern(&mut pattern, 0, base);
-    fill_pattern(&mut pattern, 1, base);
-    fill_pattern(&mut pattern, 0, base);
-    fill_pattern(&mut pattern, -1, base);
-    while ret.len() <= n {
-        ret.extend(&pattern);
-    }
-    ret.pop_front();
-    ret.truncate(n);
-    ret.iter().map(|x| *x).collect()
-}
-
-fn apply_pattern(input: &Vec<isize>, pattern: &Vec<isize>) -> isize {
+fn apply_pattern(input: &Vec<isize>, step: usize, start_from: usize) -> isize {
     let mut res: isize = 0;
-    for i in 0..input.len() {
-        res += input[i] * pattern[i]
+    for i in start_from..input.len() {
+        res += input[i] * get_pattern_val(step, i);
     }
     res.abs() % 10
 }
 
-fn apply_phase(input: Vec<isize>) -> Vec<isize> {
+fn apply_phase(input: Vec<isize>, start_from: usize) -> Vec<isize> {
     let size = input.len();
     let mut ret: Vec<isize> = Vec::new();
-    for step in 0..size {
-        let pattern = get_pattern(step, size);
-        let val = apply_pattern(&input, &pattern);
+    for step in start_from..size {
+        let val = apply_pattern(&input, step, start_from);
         ret.push(val);
     }
     ret
@@ -49,7 +39,7 @@ pub fn part1(mut input: String) {
         .map(|x| x.to_string().parse::<isize>().unwrap())
         .collect::<Vec<isize>>();
     for _ in 0..100 {
-        curr = apply_phase(curr)
+        curr = apply_phase(curr, 0)
     }
 
     curr.truncate(8);
@@ -61,25 +51,63 @@ pub fn part1(mut input: String) {
     println!("Solution part 1: {:?}", sol);
 }
 
+fn to_number(input: &[isize]) -> isize {
+    input.iter().fold(0, |total, current| total * 10 + current)
+}
+
+// OK, I didn't get this by myself, this is adapterd from https://github.com/prscoelho/aoc2019/blob/master/src/aoc16/mod.rs
+pub fn part2(mut input: String) {
+    input.pop(); // removes the trailing \n
+    let curr = input
+        .chars()
+        .map(|x| x.to_string().parse::<isize>().unwrap())
+        .collect::<Vec<isize>>();
+    let start = to_number(&curr[0..7]) as usize;
+    let end = curr.len() * 10_000;
+    let mut current = Vec::new();
+    for i in start..end {
+        current.push(curr[i % curr.len()]);
+    }
+
+    for _ in 0..100 {
+        let mut sums = Vec::new();
+        let mut total = 0;
+        sums.push(0);
+        for i in 0..current.len() {
+            total += current[i];
+            sums.push(total);
+        }
+        for i in 0..current.len() {
+            let value = sums.last().unwrap() - sums[i];
+            current[i] = value % 10;
+        }
+    }
+    let res = to_number(&current[0..8]);
+    println!("Solution part 2: {:?}", res);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn day16_part1() {
-        let p0 = get_pattern(0, 8);
-        assert_eq!(p0, vec![1, 0, -1, 0, 1, 0, -1, 0]);
-
-        let p1 = get_pattern(0, 10);
-        assert_eq!(p1, vec![1, 0, -1, 0, 1, 0, -1, 0, 1, 0]);
-
-        let p2 = get_pattern(1, 10);
-        assert_eq!(p2, vec![0, 1, 1, 0, 0, -1, -1, 0, 0, 1]);
-
-        let r1 = apply_pattern(
-            &vec![1, 2, 3, 4, 5, 6, 7, 8],
-            &vec![1, 0, -1, 0, 1, 0, -1, 0],
-        );
-        assert_eq!(r1, 4);
+        assert_eq!(get_pattern_val(0, 0), 1);
+        assert_eq!(get_pattern_val(0, 1), 0);
+        assert_eq!(get_pattern_val(0, 2), -1);
+        assert_eq!(get_pattern_val(0, 3), 0);
+        assert_eq!(get_pattern_val(0, 4), 1);
+        assert_eq!(get_pattern_val(1, 0), 0);
+        assert_eq!(get_pattern_val(1, 1), 1);
+        assert_eq!(get_pattern_val(1, 2), 1);
+        assert_eq!(get_pattern_val(1, 3), 0);
+        assert_eq!(get_pattern_val(1, 4), 0);
+        assert_eq!(get_pattern_val(1, 5), -1);
+        assert_eq!(get_pattern_val(2, 0), 0);
+        assert_eq!(get_pattern_val(2, 1), 0);
+        assert_eq!(get_pattern_val(2, 2), 1);
+        assert_eq!(get_pattern_val(2, 3), 1);
+        assert_eq!(get_pattern_val(2, 4), 1);
+        assert_eq!(get_pattern_val(2, 5), 0);
     }
 }
